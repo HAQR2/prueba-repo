780579b56ef39fd16dfe08e0edd42482c1674c01
hector Quintero <hector225@gmail.com>
2022-11-08 09:34:49 -0300

    cambios

---
+++ javascript/index.js
@@ -0,0 +1,94 @@
+/*  
+console.log(typeof {})
+
+let nombre = "Hector";
+let apellido = "Quintero";
+
+let edad = 34;
+let mascota = "No"
+
+let nombreApellido = nombre + " " + apellido;
+console.log(nombreApellido);
+
+let nombreIngresado = prompt("ingrese su nombre")
+
+alert ("Registro Exitoso!")
+
+let entrada = prompt("ingrese una letra")
+let salida = entrada + " " + "ingresado";
+
+alert(salida)
+console.log(salida) */
+
+
+/* console.log(nombre + " " + apellido)
+console.log(apellido)
+console.log(edad)
+console.log(mascota) */
+
+/* let numeroUno = 5;
+
+let numeroDos = 10;
+
+let resultadoSuma = numeroUno + numeroDos;
+let resultadoResta = numeroUno - numeroDos;
+let resultadoMulti = numeroUno * numeroDos;
+let resultadoDiv = numeroUno / numeroDos;
+
+
+/* let entrynumber = prompt("ingrese un valor numerico");
+if (a > 5) {
+
+    console.log("El número es mayor a 5");
+
+} else {
+    console.log("El número es menor o igual 5");
+}
+ */
+
+/* let entrynumber = prompt("ingrese un valor numerico: ");
+
+if (entrynumber > 1000) {
+
+    alert("el numero es mayor a 1000");
+    
+} else {
+    alert("el numero es menor o igual a 1000");
+} */
+
+/* let arrayStrings = [
+    ['dato1', 'dato2', 'dato3'],
+    ['Perro1', 'gato2', 'animal3'],
+    ['prueba', 'perico', 'leche']
+ ]
+
+console.log(arrayStrings);
+console.log(arrayStrings.length)
+console.log(arrayStrings[0])
+
+ */
+
+/* let articleList = ["leche", "Pomelo", "naranja",];
+let articleList = prompt("ingrese un articulo:");
+
+if (!articleList) {
+    alert(" no ha ingresado un articulo");
+} else if (articleList.includes(articleEntry)) {
+    alert("el producto ya esta en l lista");
+} else {
+    articleList.push(articleEntry);
+    alert("La lista de productos contiene: ") + articleList);
+    console.log(articleList)
+    } */
+let entrynumber = prompt("ingrese un numero: ");
+if (entrynumber >10) {
+    console.log(" el valor ingresado es mayor a 10")
+    document.write("el valor ingresado es mayor a 10")
+} else if (entrynumber <= 10) {
+} else {
+
+}
+    
+
+
+
